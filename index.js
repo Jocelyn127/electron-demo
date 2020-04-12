8786103b37619a3cc888a8c0471662c9830b84a8
jocelyn <1270264022@qq.com>
2020-04-13 00:41:27 +0800

    [electron_demo] add top menu and dev tools

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 // import electron instance
 const electron = require('electron');
-const { app, BrowserWindow } = electron;
+const { app, BrowserWindow, Menu } = electron;
 const path = require('path');
-const url=require('url');
+const url = require('url');
 
 let win = null;
 // listen
@@ -12,10 +12,50 @@ app.on('ready', () => {
         height: 600,
         width: 600
     });
+
     // win.loadURL('https://www.baidu.com');
     win.loadURL(url.format({
         pathname: path.resolve(__dirname, './html/main.html'),
         proctocal: 'file',
         slashes: true
     }));
-}) 
+
+    //Define nav bar
+    const mainMenu = Menu.buildFromTemplate(menuTemplate);
+    Menu.setApplicationMenu(mainMenu);
+});
+
+// Top menu template
+const menuTemplate = [{
+    // File menu items
+    label: 'File',
+    submenu: [
+        { label: 'Insert Info' },
+        { label: 'Delete Info' },
+        {
+            label: 'Quit',
+            accelerator: process.platform === 'darwin' ? 'Command+Q' : 'Ctrl+Q',
+            click: () => {
+                // quit window
+                app.quit();
+            }
+        }
+    ]
+}, {
+    label: 'Dev Tools',
+    submenu: [
+        {
+            label: 'Open/Close',
+            accelerator: process.platform==='darwin'?'':'F12',
+            click: (item, focusedWindow) => {
+                // current focused window, toggle devtools
+                focusedWindow.toggleDevTools();
+            }
+        },
+        {
+            label: 'Refresh',
+            role: 'reload',
+            accelerator: process.platform === 'darwin' ? 'Command+F5' : 'Ctrl+F5'
+        }
+    ]
+}];
